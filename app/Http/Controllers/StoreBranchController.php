3507a143d4b4e125db61b8630c0d8fe4a0ae1330
FOR3ST321 <mkelvin3321@gmail.com>
2021-12-08 21:15:21 +0700

    Migration Store Branch

---
+++ app/Http/Controllers/StoreBranchController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Store_Branch;
+use App\Http\Requests\StoreStore_BranchRequest;
+use App\Http\Requests\UpdateStore_BranchRequest;
+
+class StoreBranchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreStore_BranchRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreStore_BranchRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Store_Branch  $store_Branch
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Store_Branch $store_Branch)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Store_Branch  $store_Branch
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Store_Branch $store_Branch)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateStore_BranchRequest  $request
+     * @param  \App\Models\Store_Branch  $store_Branch
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateStore_BranchRequest $request, Store_Branch $store_Branch)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Store_Branch  $store_Branch
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Store_Branch $store_Branch)
+    {
+        //
+    }
+}
