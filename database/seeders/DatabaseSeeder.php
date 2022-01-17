cb2e96cbe76796d7c5d0b63ebf0dee85fb48b8e3
christopherkesuma <dnlkusuma1@gmail.com>
2022-01-17 11:32:58 +0700

    masukin img package

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -78,7 +78,7 @@ class DatabaseSeeder extends Seeder
             'description' => 'This is the cheapest package we got!<br>
             But being the cheapest package doesn\'t mean that the experience will also be cheap.<br>
             You can do basic task in this package smoothly.',
-            'photo_url' => 'https://media.gettyimages.com/photos/streaming-live-esport-event-on-computer-at-home-picture-id1190641416?k=20&m=1190641416&s=612x612&w=0&h=GDhOS17zD0Liylkf7j3Xhf7gl96J58id-LV3o5ah4ag='
+            'photo_url' => 'http://localhost:8000/frontend/img/package-img/hemat.jpg'
         ]);
 
         Package::create([
@@ -93,7 +93,7 @@ class DatabaseSeeder extends Seeder
             'description' => 'This package is for you student out there!<br>
             This package is enough for your basic learning.<br>
             Webcam and Mic is included for online meeting.',
-            'photo_url' => 'https://media.gettyimages.com/photos/classic-computer-classroom-picture-id1145371232?k=20&m=1145371232&s=612x612&w=0&h=pfcK32cfHzfuZNCokUdhiLbpijjQr4OwvXlpegEKV7g='
+            'photo_url' => 'http://localhost:8000/frontend/img/package-img/learning.jpg'
         ]);
 
         Package::create([
@@ -107,7 +107,7 @@ class DatabaseSeeder extends Seeder
             Windows 10 Operating System',
             'description' => 'This package is for you who wants to do basic computing stuff and light gaming.<br>
             It\'s worth the price for the performance that you can get from this package!',
-            'photo_url' => 'https://media.gettyimages.com/photos/gamer-room-picture-id1311350206?k=20&m=1311350206&s=612x612&w=0&h=RJM19owwEk8BcaemUSNB8pjjV4uNDuccjQ67sAaVLKs='
+            'photo_url' => 'http://localhost:8000/frontend/img/package-img/regular.jpg'
         ]);
         Package::create([
             'package_name' => 'Gaming',
@@ -121,7 +121,7 @@ class DatabaseSeeder extends Seeder
             'description' => 'This package is for you gamers out there!<br>
             Play demanding games with ease in this package!
             Included with mic for better voice chat in online games!',
-            'photo_url' => 'https://media.gettyimages.com/photos/gamer-room-picture-id1311350206?k=20&m=1311350206&s=612x612&w=0&h=RJM19owwEk8BcaemUSNB8pjjV4uNDuccjQ67sAaVLKs='
+            'photo_url' => 'http://localhost:8000/frontend/img/package-img/gaming.jpg'
         ]);
         Package::create([
             'package_name' => 'Gaming Pro',
@@ -135,7 +135,7 @@ class DatabaseSeeder extends Seeder
             'description' => 'This package is for you hardcore gamers out there!<br>
             Play AAA games with ease in this package!
             Included with mic for better voice chat in online games!',
-            'photo_url' => 'https://media.gettyimages.com/photos/gamer-room-picture-id1311350206?k=20&m=1311350206&s=612x612&w=0&h=RJM19owwEk8BcaemUSNB8pjjV4uNDuccjQ67sAaVLKs='
+            'photo_url' => 'http://localhost:8000/frontend/img/package-img/gaming-pro.jpg'
         ]);
         Package::create([
             'package_name' => 'Sultan',
@@ -149,7 +149,7 @@ class DatabaseSeeder extends Seeder
             'description' => 'This package is for you who want the best hardware out there!<br>
             Capable to do heavy task with ease!<br>
             Included with camera and mic for streaming or other things!',
-            'photo_url' => 'https://media.gettyimages.com/photos/gamer-room-picture-id1311350206?k=20&m=1311350206&s=612x612&w=0&h=RJM19owwEk8BcaemUSNB8pjjV4uNDuccjQ67sAaVLKs='
+            'photo_url' => 'http://localhost:8000/frontend/img/package-img/sultan.jpg'
         ]);
         
         StoreBranch::create([
